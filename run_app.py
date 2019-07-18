fe9c6299d81c15f3d9a4e55dde940c9e8dc3c4c7
motiejuskasp <32435225+motiejuskasp@users.noreply.github.com>
2019-07-18 11:56:38 +0300

    Update run_app.py

--- run_app.py
+++ run_app.py
@@ -6,9 +6,10 @@ from rasa_slack_connector import SlackInput
 nlu_interpreter = RasaNLUInterpreter('./models/nlu/default/movienlu')
 agent = Agent.load('./models/dialogue', interpreter = nlu_interpreter)
 
-input_channel = SlackInput('xoxp-366026811859-365582762801-366266745605-4af5546fae51fc8cb936be5e2203a6a8',
-                            'xoxb-366026811859-366128665410-YSgFCjBueDOVQIvhB2L1OlxL',
-                            'vaUyndScYTpL6Jn7ShfscHOd', True)
+# enter your own token codes
+input_channel = SlackInput('OAuthAccessToken',
+                            'BotUserOAuthAccessToken',
+                            'VerificationToken', True)
 
 agent.handle_channel(HttpInputChannel(5004, '/', input_channel))
 
