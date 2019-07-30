e2ff4c6fc6e01ceb47db86296d37d53e514976f8
motiejuskasp <32435225+motiejuskasp@users.noreply.github.com>
2019-07-30 21:58:28 +0300

    Update tmdb_test.py

--- test/tmdb_test.py
+++ test/tmdb_test.py
@@ -4,7 +4,8 @@ conn = http.client.HTTPSConnection("api.themoviedb.org")
 
 payload = "{}"
 
-conn.request("GET", "/3/search/person?include_adult=false&page=1&query=Ian%20McKellen&language=en-US&api_key=4e451d47079417d82742022a3cf54988", payload)
+# use your key 
+conn.request("GET", "/3/search/person?include_adult=false&page=1&query=Ian%20McKellen&language=en-US&api_key=YOURKEYHERE", payload)
 
 res = conn.getresponse()
 data = res.read()
